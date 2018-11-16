11141d3d5e1ed6a6ae4cc85bcd1f95668e7079e4
Md.Meraj Ali <meraj09034@gmail.com>
2018-11-17 02:41:32 +0600

    Hello
    First apps

---
+++ string/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+    int c;
+    string a;
+    string b;
+     cin>>c;
+
+    getline(cin,a);
+    getline(cin,a);
+    getline(cin,b);
+
+    cout<<"  a  "<<a<<endl;
+    cout<<" b "<< b<<endl;
+    cout<<"c "<< c<<endl;
+
+}
